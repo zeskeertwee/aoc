0597f967d6d68badb74a98c76c39863b494f04dc
Jasper Fortuin <zeskeertwee@gmail.com>
2024-12-03 10:13:55 +0100

    day 1 2024

---
+++ aoc-2024/src/day1.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+fn parse_input_day1(input: &str) -> (Vec<u32>, Vec<u32>) {
+    input.lines()
+        .map(|v| { v.split_ascii_whitespace().map(|v| v.parse().unwrap()).collect() })
+        .fold((Vec::new(), Vec::new()), |(mut left, mut right), i: Vec<u32>| {
+            left.push(i[0]);
+            right.push(i[1]);
+            (left, right)
+        })
+}
+
+#[aoc(day1, part1)]
+fn part1(input: &(Vec<u32>, Vec<u32>)) -> u32 {
+    let (mut left, mut right) = input.clone();
+
+    left.sort();
+    right.sort();
+
+    let mut count = 0;
+
+    for i in 0..left.len() {
+        count += left[i].abs_diff(right[i]);
+    }
+
+    return count;
+}
+
+#[aoc(day1, part2)]
+fn part2(input: &(Vec<u32>, Vec<u32>)) -> u32 {
+    let mut left = HashMap::new();
+
+    for i in input.1.iter() {
+        let v = if let Some(val) = left.get(&i) {
+            val + 1
+        } else {
+            1
+        };
+
+        left.insert(i, v);
+    }
+
+    let mut sum = 0;
+
+    for i in input.0.iter() {
+        sum += left.get(&i).unwrap_or(&0) * i;
+    }
+
+    sum
+}
