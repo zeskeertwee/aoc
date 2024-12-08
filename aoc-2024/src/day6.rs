b7cf7c9973cbfb543c897cb4be5899973b8107e4
Jasper Fortuin <zeskeertwee@gmail.com>
2024-12-08 17:24:11 +0100

    Use Vector2 in day 6

--- aoc-2024/src/day6.rs
+++ aoc-2024/src/day6.rs
@@ -1,23 +1,24 @@
 use std::collections::HashSet;
 use aoc_runner_derive::{aoc, aoc_generator};
 use rayon::prelude::*;
+use crate::util::Vector2;
 
-type Direction = (isize, isize);
+type Direction = Vector2;
 struct Input {
     map: Vec<Vec<char>>,
     // y,x coord of starting position
-    starting_pos: (isize, isize)
+    starting_pos: Vector2
 }
 
 #[aoc_generator(day6)]
 fn parse_input(input: &str) -> Input {
-    let mut starting_pos = (0,0);
+    let mut starting_pos = Vector2::new(0, 0);
 
     Input {
         map: input.lines().enumerate()
             .map(|(y, l)| {
                 if let Some(x) = l.find('^') {
-                    starting_pos = (y as isize, x as isize)
+                    starting_pos = Vector2::new(x as i64, y as i64)
                 }
 
                 l.chars().collect()
@@ -29,29 +30,23 @@ fn parse_input(input: &str) -> Input {
 
 #[aoc(day6, part1)]
 fn part1(input: &Input) -> usize {
-    run_through_map(input.starting_pos, &input.map, usize::MAX).0.len()
+    run_through_map(input.starting_pos, &input.map).0.len()
 }
 
 #[aoc(day6, part2)]
 fn part2(input: &Input) -> usize {
-    let visited_positions = run_through_map(input.starting_pos, &input.map, usize::MAX).0;
+    let visited_positions = run_through_map(input.starting_pos, &input.map).0;
 
-    visited_positions.into_par_iter().map(|(y,x)| {
+    visited_positions.into_par_iter().map(|pos| {
         let mut grid  = input.map.clone();
-        grid[y as usize][x as usize] = '#';
+        grid[pos.y as usize][pos.x as usize] = '#';
 
-        if run_through_map(input.starting_pos, &grid, usize::MAX).1 == usize::MAX {
-            1
-        } else {
-            0
-        }
+        run_through_map(input.starting_pos, &grid).1 as usize
     }).sum()
 }
 
 #[aoc(day6, part2, naive)]
 fn part2_naive(input: &Input) -> usize {
-    let upper_limit = input.map.len() * input.map[0].len(); // if we walk as many squares as the grid has, we probably hit a loop, right?
-
     (0..input.map.len()).into_par_iter().map(|y| {
         (0..input.map[0].len()).into_par_iter().map(|x| {
             if input.map[y][x] == '#' {
@@ -60,63 +55,51 @@ fn part2_naive(input: &Input) -> usize {
                 let mut grid  = input.map.clone();
                 grid[y][x] = '#';
 
-                if run_through_map(input.starting_pos, &grid, upper_limit).1 == upper_limit {
-                    1
-                } else {
-                    0
-                }
+                run_through_map(input.starting_pos, &grid).1 as usize
             }
         }).sum::<usize>()
     }).sum()
 }
 
-// returns visited squares, steps
-fn run_through_map(starting_pos: (isize, isize), map: &Vec<Vec<char>>, max_steps: usize) -> (HashSet<(isize, isize)>, usize) {
-    let mut position = starting_pos;
-    let mut direction = (-1, 0); // starting direction up
-    let mut steps = 0;
+// returns visited squares, hit loop
+fn run_through_map(starting_pos: Vector2, map: &Vec<Vec<char>>) -> (HashSet<Vector2>, bool) {
+    let map_width = map.len() as i64;
+    let map_height = map[0].len() as i64;
 
-    let mut visited: HashSet<(isize, isize)> = HashSet::new();
+    let mut position = starting_pos;
+    let mut direction = Vector2::new(0, -1); // starting direction up
+
+    let mut visited: HashSet<Vector2> = HashSet::new();
     visited.insert(position); // include starting position
 
-    let mut rot_pos: HashSet<(isize, isize, Direction)> = HashSet::new();
+    let mut rot_pos: HashSet<(Vector2, Direction)> = HashSet::new();
 
     loop {
-        let next_position = (position.0 + direction.0, position.1 + direction.1);
-        if next_position.0 < 0 || next_position.1 < 0 || next_position.0 as usize >= map.len() || next_position.1 as usize >= map[0].len() {
-            //for l in grid {
-            //    println!("{}", l.iter().collect::<String>());
-            //}
+        let next_position = position + direction;
+        if next_position.x < 0 || next_position.y < 0 || next_position.x >= map_width || next_position.y >= map_height {
             // fall off the map
-            return (visited, steps);
+            return (visited, false);
         }
 
-        if map[next_position.0 as usize][next_position.1 as usize] == '#' {
-            if !rot_pos.insert((position.0, position.1, direction)) {
+        if map[next_position.y as usize][next_position.x as usize] == '#' {
+            if !rot_pos.insert((position, direction)) {
                 // loop, we already were here in the same direction
-                //dbg!(steps);
-                return (visited, max_steps);
+                return (visited, true);
             }
             direction = rot_direction(direction);
         } else {
-            steps += 1;
-
             position = next_position;
             visited.insert(position);
-
-            if steps == max_steps {
-                return (visited, steps);
-            }
         }
     }
 }
 
 fn rot_direction(dir: Direction) -> Direction {
-    match dir {
-        (-1, 0) => (0, 1), // up -> right
-        (0, 1) => (1, 0), // right -> down
-        (1, 0) => (0, -1), // down -> left
-        (0, -1) => (-1, 0), // left -> up
+    match (dir.x, dir.y) {
+        (0, -1) => Vector2::new(1, 0), // up -> right
+        (1, 0) => Vector2::new(0, 1), // right -> down
+        (0, 1) => Vector2::new(-1, 0), // down -> left
+        (-1, 0) => Vector2::new(0, -1), // left -> up
         _ => panic!("Unexpected direction!")
     }
 }
