7cb747a4e6b579cbeb5c11b59a8c0f2a59af6c9e
Jasper Fortuin <zeskeertwee@gmail.com>
2024-12-09 20:49:57 +0100

    Optimize 6-2

--- aoc-2024/src/day6.rs
+++ aoc-2024/src/day6.rs
@@ -30,18 +30,18 @@ fn parse_input(input: &str) -> Input {
 
 #[aoc(day6, part1)]
 fn part1(input: &Input) -> usize {
-    run_through_map(input.starting_pos, &input.map).0.len()
+    run_through_map_p1(input.starting_pos, &input.map).len()
 }
 
 #[aoc(day6, part2)]
 fn part2(input: &Input) -> usize {
-    let visited_positions = run_through_map(input.starting_pos, &input.map).0;
+    let visited_positions = run_through_map_p1(input.starting_pos, &input.map);
 
     visited_positions.into_par_iter().map(|pos| {
         let mut grid  = input.map.clone();
         grid[pos.y as usize][pos.x as usize] = '#';
 
-        run_through_map(input.starting_pos, &grid).1 as usize
+        run_through_map_detect_loop(input.starting_pos, &grid) as usize
     }).sum()
 }
 
@@ -55,14 +55,13 @@ fn part2_naive(input: &Input) -> usize {
                 let mut grid  = input.map.clone();
                 grid[y][x] = '#';
 
-                run_through_map(input.starting_pos, &grid).1 as usize
+                run_through_map_detect_loop(input.starting_pos, &grid) as usize
             }
         }).sum::<usize>()
     }).sum()
 }
 
-// returns visited squares, hit loop
-fn run_through_map(starting_pos: Vector2, map: &Vec<Vec<char>>) -> (HashSet<Vector2>, bool) {
+fn run_through_map_p1(starting_pos: Vector2, map: &Vec<Vec<char>>) -> HashSet<Vector2> {
     let map_width = map.len() as i64;
     let map_height = map[0].len() as i64;
 
@@ -72,25 +71,55 @@ fn run_through_map(starting_pos: Vector2, map: &Vec<Vec<char>>) -> (HashSet<Vect
     let mut visited: HashSet<Vector2> = HashSet::new();
     visited.insert(position); // include starting position
 
+    loop {
+        let next_position = position + direction;
+        if next_position.x < 0 || next_position.y < 0 || next_position.x >= map_width || next_position.y >= map_height {
+            // fall off the map
+            return visited;
+        }
+
+        if map[next_position.y as usize][next_position.x as usize] == '#' {
+            direction = rot_direction(direction);
+        } else {
+            position = next_position;
+            visited.insert(position);
+        }
+    }
+}
+
+// returns visited squares, hit loop
+fn run_through_map_detect_loop(starting_pos: Vector2, map: &Vec<Vec<char>>) -> bool {
+    let map_width = map.len() as i64;
+    let map_height = map[0].len() as i64;
+
+    let mut position = starting_pos;
+    let mut direction = Vector2::new(0, -1); // starting direction up
+
     let mut rot_pos: HashSet<(Vector2, Direction)> = HashSet::new();
 
     loop {
         let next_position = position + direction;
         if next_position.x < 0 || next_position.y < 0 || next_position.x >= map_width || next_position.y >= map_height {
             // fall off the map
-            return (visited, false);
+            return false;
         }
 
-        if map[next_position.y as usize][next_position.x as usize] == '#' {
-            if !rot_pos.insert((position, direction)) {
-                // loop, we already were here in the same direction
-                return (visited, true);
+        let mut scan_pos = next_position;
+        while map[scan_pos.y as usize][scan_pos.x as usize] != '#' {
+            scan_pos = scan_pos + direction;
+            if scan_pos.x < 0 || scan_pos.y < 0 || scan_pos.x >= map_width || scan_pos.y >= map_height {
+                // fall off the map
+                return false;
             }
-            direction = rot_direction(direction);
-        } else {
-            position = next_position;
-            visited.insert(position);
         }
+
+        if !rot_pos.insert((scan_pos, direction)) {
+            // loop, we already were here in the same direction
+            return true;
+        }
+
+        position = scan_pos - direction;
+        direction = rot_direction(direction);
     }
 }
 
