b432f86f5f1489bdcbb0621eaab7bfabd4635a80
Jasper Fortuin <zeskeertwee@gmail.com>
2025-11-12 08:46:52 +0100

    Add memoizer to aoclib

---
+++ aoclib/src/memoizer.rs
@@ -0,0 +1,48 @@
+use std::hash::Hash;
+use rustc_hash::FxHashMap;
+
+pub struct Memoizer<A, R> {
+    cache: FxHashMap<A, R>,
+    func: Box<dyn Fn(&A) -> R>,
+}
+
+impl<A: Hash + Clone + Eq, R: Clone> Memoizer<A, R> {
+    pub fn new(func: Box<dyn Fn(&A) -> R>) -> Memoizer<A, R> {
+        Memoizer {
+            cache: FxHashMap::default(),
+            func
+        }
+    }
+
+    pub fn call(&mut self, arg: A) -> R {
+        if let Some(v) = self.cache.get(&arg) {
+            return v.clone();
+        }
+
+        let v = (self.func)(&arg);
+        self.cache.insert(arg.clone(), v.clone());
+        v
+    }
+}
+
+#[test]
+fn memoize_test() {
+    let mut mem = Memoizer::new(Box::new(crate::memoizer::testfunc::test_func));
+    assert_eq!(mem.call(()), 42);
+    assert_eq!(mem.call(()), 42);
+    assert_eq!(mem.call(()), 42);
+}
+
+#[cfg(test)]
+mod testfunc {
+    use std::sync::atomic::AtomicBool;
+    use std::sync::atomic::Ordering;
+
+    const BOOL: AtomicBool = AtomicBool::new(false);
+
+    pub fn test_func(arg: &()) -> u8 {
+        assert!(!BOOL.load(Ordering::Relaxed));
+        BOOL.store(true, Ordering::Relaxed);
+        42
+    }
+}
