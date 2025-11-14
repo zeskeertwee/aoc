e4647372921c2033de79b8ae54600eb03d81e05a
Jasper Fortuin <zeskeertwee@gmail.com>
2025-11-14 10:33:35 +0100

    2015 day 1: Optimize

--- aoc-2015/src/day1.rs
+++ aoc-2015/src/day1.rs
@@ -2,39 +2,27 @@ use aoc_runner_derive::{aoc, aoc_generator};
 use aoclib::aoc_test;
 
 #[aoc_generator(day1)]
-fn parse_input(input: &str) -> String {
-    input.to_string()
+fn parse_input(input: &str) -> Vec<i32> {
+    input.chars().map(|c| if c == '(' { 1 } else { -1 }).collect()
 }
 
 #[aoc(day1, part1)]
-fn part1(input: &str) -> i64 {
-    input.chars()
-        .fold(0, |acc, c| {
-            match c {
-                '(' => acc + 1,
-                ')' => acc - 1,
-                _ => panic!()
-            }
-        })
+fn part1(input: &[i32]) -> i32 {
+    input.iter()
+        .sum()
 }
 
 #[aoc(day1, part2)]
-fn part2(input: &str) -> usize {
+fn part2(input: &[i32]) -> usize {
     let mut floor = 0;
 
-    for (idx, c) in input.chars().enumerate() {
-        match c {
-            '(' => floor += 1,
-            ')' => floor -= 1,
-            _ => panic!()
-        };
-
-        if floor == -1 {
-            return idx + 1;
-        }
-    }
-
-    return 0;
+    input.iter()
+        .position(|v| {
+            floor += v;
+            floor < 0
+        })
+        .map(|f| f + 1)
+        .unwrap()
 }
 
 aoc_test!(test_day1, "../input/2015/day1.txt", 232, 1783);
