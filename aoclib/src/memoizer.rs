9f5870f64fad0303e58b1affd777827ef304bc4b
Jasper Fortuin <zeskeertwee@gmail.com>
2025-12-18 10:41:28 +0100

    aoclib: modify memorizer for recursion and add uncached static input argument

--- aoclib/src/memoizer.rs
+++ aoclib/src/memoizer.rs
@@ -1,33 +1,36 @@
 use std::hash::Hash;
+use std::sync::Mutex;
 use rustc_hash::FxHashMap;
 
-pub struct Memoizer<A, R> {
-    cache: FxHashMap<A, R>,
-    func: Box<dyn Fn(&A) -> R>,
+pub struct Memoizer<A, B, R> {
+    fixed_input: B,
+    cache: Mutex<FxHashMap<A, R>>,
+    func: Box<dyn Fn(&A, &B, &Memoizer<A, B, R>) -> R>,
 }
 
-impl<A: Hash + Clone + Eq, R: Clone> Memoizer<A, R> {
-    pub fn new(func: Box<dyn Fn(&A) -> R>) -> Memoizer<A, R> {
+impl<A: Hash + Clone + Eq, B, R: Clone> Memoizer<A, B, R> {
+    pub fn new(func: Box<dyn Fn(&A, &B, &Memoizer<A, B, R>) -> R>, fixed_input: B) -> Memoizer<A, B, R> {
         Memoizer {
-            cache: FxHashMap::default(),
+            cache: Mutex::new(FxHashMap::default()),
+            fixed_input,
             func
         }
     }
 
-    pub fn call(&mut self, arg: A) -> R {
-        if let Some(v) = self.cache.get(&arg) {
+    pub fn call(&self, arg: A) -> R {
+        if let Some(v) = self.cache.lock().unwrap().get(&arg) {
             return v.clone();
         }
 
-        let v = (self.func)(&arg);
-        self.cache.insert(arg.clone(), v.clone());
+        let v = (self.func)(&arg, &self.fixed_input, &self);
+        self.cache.lock().unwrap().insert(arg.clone(), v.clone());
         v
     }
 }
 
 #[test]
 fn memoize_test() {
-    let mut mem = Memoizer::new(Box::new(crate::memoizer::testfunc::test_func));
+    let mut mem = Memoizer::new(Box::new(crate::memoizer::testfunc::test_func), ());
     assert_eq!(mem.call(()), 42);
     assert_eq!(mem.call(()), 42);
     assert_eq!(mem.call(()), 42);
@@ -37,10 +40,11 @@ fn memoize_test() {
 mod testfunc {
     use std::sync::atomic::AtomicBool;
     use std::sync::atomic::Ordering;
+    use crate::memoizer::Memoizer;
 
     const BOOL: AtomicBool = AtomicBool::new(false);
 
-    pub fn test_func(_arg: &()) -> u8 {
+    pub fn test_func(_arg: &(), _fixed_input: &(), mem: &Memoizer<(), (), u8>) -> u8 {
         assert!(!BOOL.load(Ordering::Relaxed));
         BOOL.store(true, Ordering::Relaxed);
         42
